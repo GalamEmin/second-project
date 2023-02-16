4b03301a5ad120d568629ddcf4ba37b70f349172
GalamEmin <galamsb@mail.ru>
2023-02-17 00:54:35 +0600

    fix1vek

---
+++ sendmail.php
@@ -0,0 +1,62 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('pgroupskz@mail.ru', 'Site');
+	//Кому отправить
+	$mail->addAddress('pgroupskz@mail.ru');
+	//Тема письма
+	$mail->Subject = 'Письмо с сайта';
+
+	//Тело письма
+	$body = '<h1>Сообщение с сайта</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['phone']))){
+		$body.='<p><strong>Номер:</strong> '.$_POST['phone'].'</p>';
+	}
+
+	if(trim(!empty($_POST['mail']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['mail'].'</p>';
+	}
+	
+	/*
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
